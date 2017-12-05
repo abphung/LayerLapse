709440249898d91b528bdca9bc843b32bf67832a
Andrew Phung <andrewphung@Andrews-MacBook-Pro.local>
2017-12-05 00:00:20 -0600

    rewrote tkinter test to load files. pygame test is now irrelevent

--- Tkinter_test.py
+++ Tkinter_test.py
@@ -1,7 +1,35 @@
-# code copied from: https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
-from Tkinter import Tk
+from Tkinter import *
 from tkFileDialog import askopenfilename
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
+class GUI:
+	
+	def __init__(self):
+		self.master = Tk()
+		self.video_button = Button(self.master, text="Chose a video", command=self.video_command)
+		self.song_button = Button(self.master, text="Chose a song", command=self.song_command)
+		self.continue_button = None
+		self.video_button.pack()
+		self.song_button.pack()
+		self.video_file = None
+		self.song_file = None
+		self.check_files()
+		self.master.mainloop()
+
+	def video_command(self):
+		self.video_file = askopenfilename()
+		self.video_button.config(text=self.video_file)
+
+	def song_command(self):
+		self.song_file = askopenfilename()
+		self.song_button.config(text=self.song_file)
+
+	def check_files(self):
+		if self.video_file != None and self.song_file != None and self.continue_button == None:
+			self.continue_button = Button(self.master, text="Continue", command=self.process) 
+			self.continue_button.pack()
+		self.master.after(1, self.check_files)
+
+	def process(self):
+		self.master.quit()
+		
+GUI()
